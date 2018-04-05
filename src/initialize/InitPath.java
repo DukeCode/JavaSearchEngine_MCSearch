0ac0898ca81dd07e35f11f1ec9ed4c5d4103fdfe
DukeCode <jid17@pitt.edu>
2018-04-05 00:55:12 -0400

    code initialize data retrieval

---
+++ src/initialize/InitPath.java
@@ -0,0 +1,8 @@
+package initialize;
+
+public class InitPath {
+	public static String urlPath="init//url//";// folder to store url
+	public static String resourcePath="init//resource//";// folder to store response
+	public static String parsePath="init//content//";// folder to store content
+	public static String paddingPath="init//padding//";// folder to store content
+}
---
+++ src/initialize/NoURLException.java
@@ -0,0 +1,14 @@
+package initialize;
+
+public class NoURLException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public NoURLException(String message) {
+		super(message);
+	}
+	public String getMessage() {
+		return super.getMessage();
+	}
+}
